fb8a285b07c65d069df939192e3317cb2b5f6d18
Alex <mail@alexzava.com>
2021-05-07 14:10:16 +0200

    Update generico

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var nospam = "bWFpbEBhbGV4emF2YS5jb20=";
 var menu = false;
-var cyberMode = false;
 
 window.onload = function() {
 
@@ -16,6 +15,8 @@ window.onload = function() {
 	if(title) {
 		title.classList.add("animate-neon");
 	}
+
+	runBaffle();
 };
 
 function antiSpam(e) {
@@ -45,60 +46,10 @@ function toggleMenu() {
 	}
 }
 
-/*function enableCyberMode() {
-	if(cyberMode) {
-		document.body.classList.remove("cyber");
-		cyberMode = false;
-	} else {
-		document.body.classList.add("cyber");
-		let b = baffle('.obfuscate', {
-			speed: 80,
-			characters: "█▒▓░ ░▒▓▒ █▒▓ ██/▓ ▒<▓▓░ ░░▓ ▓█/░░ ▒░█░░ ▒░▒",
-		}).start();
-		b.reveal(4000);
-		cyberMode = true;
-	}
-}*/
-
-/* XMAS */
-/*function rainDrops(size, element) {
-	var posArr = [];
-	var lastDelay = 0;
-	while(posArr.length < size){
-	    var pos = Math.floor(Math.random() * 99);
-	    if(posArr.indexOf(pos) === -1) {
-	    	posArr.push(pos);
-
-			let delay = lastDelay;
-			while(delay === lastDelay) {
-				delay = Math.random() * 8;
-			}
-			lastDelay = delay;
-
-			console.log(delay);
-
-			let drop = document.createElement('div');
-			drop.classList.add("rain-drop");
-			drop.style.left = pos + "vw";
-			drop.style.animationDelay = "-" + delay + "s";
-
-			element.append(drop);
-	    }
-	}
-}*/
-
-/*function placeSanta() {
-	let innerTitle = document.getElementById("innerTitle");
-	let santa = document.getElementById("santa");
-	if(innerTitle && santa) {
-		let titleRect = innerTitle.getBoundingClientRect();
-		let santaRect = santa.getBoundingClientRect();
-
-		let santaX = titleRect.x - (santaRect.height / 2);
-		let santaY = titleRect.y - (santaRect.width / 3);
-
-		santa.style.left = santaX + "px";
-		santa.style.top = santaY + "px";
-		santa.style.transform = "rotate(-20deg)";
-	}
-}*/
+function runBaffle() {
+	let b = baffle('.obfuscate', {
+		speed: 80,
+		characters: "█▒▓░ ░▒▓▒ █▒▓ ██/▓ ▒<▓▓░ ░░▓ ▓█/░░ ▒░█░░ ▒░▒",
+	}).start();
+	b.reveal(4000);
+}
