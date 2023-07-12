363feb2b7110ebd1140b555461b7a50602b426f4
Alexzava <9405622+Alexzava@users.noreply.github.com>
2023-07-12 14:56:13 +0200

    Website update

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const noSpamValue = "bWFpbEBhbGV4emF2YS5jb20="
+
+window.onload = function() {
+	VanillaTilt.init(document.querySelector(".tilt"), {
+		max: 10,
+		speed: 60
+	})
+
+	let b = baffle('.obfuscate', {
+		speed: 80,
+		characters: "$#/*+{}[]-_&%!?<>",
+	}).start()
+
+	b.reveal(4000)
+}
+
+function noSpam(e) {
+	e.onclick = ""
+	e.href = "mailto:" + window.atob(noSpamValue)
+}
