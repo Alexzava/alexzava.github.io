5c681214cc29aa1485709eb49d6fcda94c1dfa83
Alexzava <9405622+Alexzava@users.noreply.github.com>
2024-05-29 21:43:25 +0200

    New design update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Alex Zava
 
-Yep
+This is my personal website.
+
+## Open source resources
+
+- [Iconoir](https://github.com/iconoir-icons/iconoir)
+- [Animate.css](https://github.com/animate-css/animate.css)
+- [Shapes](https://shapes.framer.website/)
+- [Scroll-timeline Polyfill](https://github.com/flackr/scroll-timeline)
+- [JQuery](https://github.com/jquery/jquery)
--- js/app.js
+++ js/app.js
@@ -1,19 +1,95 @@
 const noSpamValue = "bWFpbEBhbGV4emF2YS5jb20="
+const animatedTextStrings = [
+	"Web Developer",
+	"Backend Developer",
+	"Full Stack Developer"
+];
+var animatedTextCounter = 0
 
-window.onload = function() {
-	VanillaTilt.init(document.querySelector(".tilt"), {
-		max: 10,
-		speed: 60
+window.onload = () => {
+	// Navbar menu button
+	let menuButton = document.getElementById("menu-btn")
+	let mobileMenuButton = document.getElementById("mobile-menu-btn")
+	if(menuButton && mobileMenuButton) {
+		menuButton.onclick = toggleNavigation
+		mobileMenuButton.onclick = toggleNavigation
+	}
+
+	// Main section animated text
+	let infoAnimatedText = document.getElementById("info-animated-text")
+	let infoAnimatedBox = document.getElementById("info-animated-box")
+	if(infoAnimatedText && infoAnimatedBox) { 
+		infoAnimatedBox.style.width = infoAnimatedText.getBoundingClientRect().width + "px"
+		infoAnimatedBox.style.height = infoAnimatedText.getBoundingClientRect().height + "px"
+
+		setInterval(() => { textScrollAnimation(infoAnimatedText) }, 3000)
+	}
+
+	// Animate horizontal scroll section
+	let section = document.querySelector('#projects')
+	let wrap = document.querySelector('.horizontal-container')
+	wrap.animate({
+		transform: ['', 'translateX(calc(-100% + 100vw))'],
+	},{
+		timeline: new ViewTimeline({
+			subject: section,
+			axis: 'block',
+		}),
+		fill: 'forwards',
+		rangeStart: 'contain 0%',
+		rangeEnd: 'contain 100%',
 	})
+}
 
-	let b = baffle('.obfuscate', {
-		speed: 80,
-		characters: "$#/*+{}[]-_&%!?<>",
-	}).start()
+function textScrollAnimation(element) {
+	$("#" + element.id).css({position: "absolute"}).animate({
+		opacity: 0,
+		top: 20
+	}, 1000, () => {
+		if(animatedTextCounter >= animatedTextStrings.length) {
+			animatedTextCounter = 0
+		}
 
-	$("#loader").fadeToggle("slow", () => {
-		b.reveal(4000)
+		element.textContent = animatedTextStrings[animatedTextCounter]
+		animatedTextCounter++
+
+		$("#" + element.id).css({top: -20}).animate({opacity: 1, top: 0}, 1000)
 	})
+}
+
+function toggleNavigation() {
+	let menuButton = document.getElementById("menu-btn")
+	let mobileMenuIcon = document.getElementById("mobile-menu-icon")
+	let navigation = document.getElementById("navigation")
+	if(window.getComputedStyle(navigation, null).display == "none") {
+		// Menu Button
+		let dataAlt = menuButton.textContent
+		menuButton.textContent = menuButton.getAttribute("data-alt")
+		menuButton.setAttribute("data-alt", dataAlt)
+
+		// Mobile Menu Button
+		mobileMenuIcon.classList.remove("iconoir-menu")
+		mobileMenuIcon.classList.add("iconoir-xmark")
+
+		// Show navigation
+		$("#" + navigation.id).css("display", "flex").hide().fadeIn()
+	} else {
+		// Menu Button
+		let dataAlt = menuButton.textContent
+		menuButton.textContent = menuButton.getAttribute("data-alt")
+		menuButton.setAttribute("data-alt", dataAlt)
+
+		// Mobile Menu Button
+		mobileMenuIcon.classList.remove("iconoir-xmark")
+		mobileMenuIcon.classList.add("iconoir-menu")
+		
+		$("#" + navigation.id).fadeOut()
+	}
+}
+
+function scrollToTop() {
+	toggleNavigation()
+	window.scrollTo({top: 0, left: 0, behavior: "smooth"})
 }
 
 function noSpam(e) {
